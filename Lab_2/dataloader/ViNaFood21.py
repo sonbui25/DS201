7c75436623f06b5426185bbb401a217d1df16422
son25 <thaisoncbq@gmail.com>
2025-10-23 20:17:54 +0700

    Increase ggnet lr

--- Lab_2/dataloader/ViNaFood21.py
+++ Lab_2/dataloader/ViNaFood21.py
@@ -25,17 +25,17 @@ class ViNaFood21Dataset(Dataset):
         if self.is_train:
             self.transform = transforms.Compose([
                 transforms.Resize((224, 224)), # Crop randomly
-                transforms.RandomHorizontalFlip(), # Flip horizontally
-                transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1), # Adjust colors
-                transforms.RandomRotation(15), # Rotate slightly
+                # transforms.RandomHorizontalFlip(), # Flip horizontally
+                # transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1), # Adjust colors
+                # transforms.RandomRotation(15), # Rotate slightly
                 transforms.ToTensor(), # Convert to tensor
-                normalize # Normalize
+                # normalize # Normalize
             ])
         else: # For validation/testing
             self.transform = transforms.Compose([
                 transforms.Resize((224, 224)), # Just resize
                 transforms.ToTensor(), # Convert to tensor
-                normalize # Normalize
+                # normalize # Normalize
             ])
 
         # --- Load all data into RAM ---
