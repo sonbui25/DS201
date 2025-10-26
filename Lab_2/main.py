7a6442d02d0c56d0ff737e17c8af219f7c7d7c9c
son25 <thaisoncbq@gmail.com>
2025-10-27 00:59:29 +0700

    Update main

--- Lab_2/main.py
+++ Lab_2/main.py
@@ -144,7 +144,7 @@ if __name__ == "__main__":
     #  DataLoaders 
     batch_size = hp['batch_size']
     num_workers = os.cpu_count() 
-    train_dataloader = DataLoader(train_data, batch_size=batch_size, collate_fn=collate_fn, num_workers=num_workers, pin_memory=True)
+    train_dataloader = DataLoader(train_data, batch_size=batch_size, shuffle=True, collate_fn=collate_fn, num_workers=num_workers, pin_memory=True)
     test_dataloader = DataLoader(test_data, batch_size=batch_size, shuffle=False, collate_fn=collate_fn, num_workers=num_workers, pin_memory=True)
 
     # Chọn Optimizer
